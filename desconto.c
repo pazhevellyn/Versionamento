731bea0f100006a5584f2917683754f669e67c07
pazhevellyn <48493287+pazhevellyn@users.noreply.github.com>
2025-05-27 15:58:28 -0300

    Add files via upload

--- desconto.c
+++ desconto.c
@@ -1,26 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int precoProd;
-
-    printf("Qual o preco do produto?");
+    printf("Digite o preco do produto: ");
     scanf("%i", &precoProd);
 
-    if(precoProd >= 100){
-        precoProd = precoProd - precoProd*0.1;
-        printf("Total com desconto: %i", precoProd);
-    }else{
-        printf("Nao teve desconto");
+    switch(precoProd){
+        case 100:
+            precoProd = precoProd - precoProd*0.1;
+            printf("Preco com desconto: %i", precoProd);
+        break;
+        case 200:
+            precoProd = precoProd - precoProd*0.2;
+            printf("Preco com desconto: %i", precoProd);
+        break;
+        case 300:
+            precoProd = precoProd - precoProd*0.3;
+            printf("Preco com desconto: %i", precoProd);
+        break;
+        case 400:
+            precoProd = precoProd - precoProd*0.4;
+            printf("Preco com desconto: %i", precoProd);
+        break;
+        case 500:
+            precoProd = precoProd - precoProd*0.5;
+            printf("Preco com desconto: %i", precoProd);
+        break;
+        default:
+            printf("Nao aplica desconto");
     }
     return 0;
 }
-
-
-
-/* Refaça o exercício do desconto adaptando para um
-desconto progressivo de 10% a 50% utilizando switch case*/
-
-
-
-
